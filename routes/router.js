7735af44adb588279cc95827de2bfb780889c028
aliaksandr-s <s-a-n-9@mail.ru>
2016-12-21 23:43:34 +0300

    Save images to local drive

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,22 @@
 const express = require('express'),
-      router = express.Router(),
-      MongoClient = require('mongodb').MongoClient,
-      config = require('../config.js'),
-      ObjectID = require('mongodb').ObjectID;
+    router = express.Router(),
+    MongoClient = require('mongodb').MongoClient,
+    config = require('../config.js'),
+    fs = require('fs'),
+    path = require('path'),
+    ObjectID = require('mongodb').ObjectID;
 
 
 router.post('/save-picture', function(req, res) {
+    // get base64 img decode it and save on disc
+    var img = req.body.img,
+        ext = img.split(';')[0].match(/jpeg|png|gif/)[0],
+        base64Data = img.replace(/^data:image\/\w+;base64,/, ""),
+        buf = new Buffer(base64Data, 'base64');
+    
+    fs.writeFile('image.' + ext, buf)
+
+    // save it to db ---- revrite to save links not the whole file
     MongoClient.connect('mongodb://localhost:27017/' + config.database, function(err, db) {
         if (!err) {
             db.collection('pictures').insert(req.body, function(err, result) {
